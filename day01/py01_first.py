9e585ec02c51c423a225ad69ac759a95528820d2
jeonjune <jyjeon0831@naver.com>
2025-01-14 17:12:13 +0900

    마무리 정리

--- day01/py01_first.py
+++ day01/py01_first.py
@@ -1,4 +1,4 @@
-# IoT 과정 첫 번째 프로그램 파일
+# IoT 과정 첫 번째 프로그램 파일 - 주석
 
-if __name__ == '__main__':
-    print("Hello, Ji Youn!")
+if __name__ == '__main__': # 조건문, __name__이 '__main__'이 같다면.
+    print("Hello, Ji Youn!") # Hello, Python을 print() 함수로 실행하라.
